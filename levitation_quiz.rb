5be083090302ff641bf52592c14395dba04f4bb4
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-06 01:35:06 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
-	#your code here
+	puts "What is the spell that enacts levitation?"
+	enswer = gets.chomp
+	break if = "Wingardium Leviosa"
 end
-
+puts "You passed the quiz!"
+end
 
