3e1f39a13ae9ec0072d47cee786e5a7a74a26677
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-06 01:35:35 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
 	puts "What is the spell that enacts levitation?"
 	enswer = gets.chomp
-	break if = "Wingardium Leviosa"
+	break if == "Wingardium Leviosa"
 end
 puts "You passed the quiz!"
 end
